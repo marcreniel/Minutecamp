ecb61e5e763cdb36037ff1aa0911393c87f94061
imedra <marcbernardino2005@gmail.com>
2024-02-17 01:24:39 -0800

    added convex backend

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { CONVEX_URL } from "@env";
+import "react-native-get-random-values";
+import Tasks from "./tasks";
+
+
+const convex = new ConvexReactClient(CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ConvexProvider client={convex}>
+        <Tasks />
+      </ConvexProvider>
     </View>
   );
 }
