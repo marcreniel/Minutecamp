9bacaafb2a330a9b91b99090a817c51ddcef1f93
Brian Ni <brianni@stanford.edu>
2024-02-18 00:32:56 -0800

    add coursesCompleted table schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,4 +7,10 @@ users: defineTable({
     name: v.string(),
     tokenIdentifier: v.string(),
   }).index("by_token", ["tokenIdentifier"]),
+
+coursesCompleted: defineTable({
+    userId: v.string(),
+    numCompleted: v.int64(),
+    courseList: v.array(v.string()),
+  }).index("by_token", ["tokenIdentifier"]),
 });
